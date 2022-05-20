74472e23ec7ae063e428178c4629afa0363656bf
Nandhakumar <nandha4083@gmail.com>
2022-05-20 15:40:38 +0000

    update

--- ops_plugin.sh
+++ ops_plugin.sh
@@ -575,8 +575,7 @@ EOF
   echo
   echo -e "\t--> Check whether Prometheus is able to scrape exporters and other components"
   prom_ip=$(kubectl get pods -n collectors -o wide | grep prometheus | awk '{print $6}')
-  prom_tgt=$(kubectl exec -it ${oc_pod_name} -n ${OC_NAMESPACE} -- /bin/sh -c "curl -s http://${prom_ip}:9090/api/v1/targets?state=active | jq --raw-output '.data.activeTargets[] | "Service: \(.discoveredLabels.__meta_kubernetes_service_name // .discoveredLabels.__meta_kuber
-netes_pod_label_opscruiseProduct), Instance: \(.labels.instance), Health: \(.health)"'")
+  prom_tgt=$(kubectl exec -it ${oc_pod_name} -n ${OC_NAMESPACE} -- /bin/sh -c "curl -s http://${prom_ip}:9090/api/v1/targets?state=active | jq --raw-output '.data.activeTargets[] | "Service: \(.discoveredLabels.__meta_kubernetes_service_name // .discoveredLabels.__meta_kubernetes_pod_label_opscruiseProduct), Instance: \(.labels.instance), Health: \(.health)"'")
   echo "$prom_tgt"
   echo
   if [ "$err_count" -gt 0 ]; then
